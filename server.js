495e723b282ddb190e1ec460be7bf4353b1262c5
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-08-31 20:55:55 +0300

    Font fix

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ let internalErrorHtml = `
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&amp;display=swap" rel="stylesheet">
         <style type="text/css">
             body, html {
-                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                font-family: "Roboto Flex", sans-serif
                 text-align: left;
                 overflow-wrap: anywhere;
                 background-color: rgba(30, 30, 30);
@@ -162,7 +162,7 @@ async function handleProxy(req, res, method) {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&amp;display=swap" rel="stylesheet">
         <style type="text/css">
             body, html {
-                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                font-family: "Roboto Flex", sans-serif
                 text-align: left;
                 overflow-wrap: anywhere;
                 background-color: rgba(30, 30, 30);
@@ -542,7 +542,7 @@ async function handleProxy(req, res, method) {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&amp;display=swap" rel="stylesheet">
         <style type="text/css">
             body, html {
-                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                font-family: "Roboto Flex", sans-serif
                 text-align: left;
                 overflow-wrap: anywhere;
                 background-color: rgba(30, 30, 30);
